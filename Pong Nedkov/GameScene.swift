699c5e5a983f5541f93717d693b0ef26a8d7f0a7
Dennis Nedkov <dnedkov9274@stu.d214.org>
2019-04-24 11:22:01 -0500

    Massive changes and improvements to the current app

--- Pong Nedkov/GameScene.swift
+++ Pong Nedkov/GameScene.swift
@@ -9,11 +9,64 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+let ballCategory: UInt32 = 1
+let topCategory: UInt32 = 2
+let paddleCategory: UInt32 = 4 // 0x1 << 5
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    
+    var topPaddle = SKSpriteNode()
+    var ball = SKSpriteNode()
+   
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody = border
         
+        let topLeft = CGPoint(x: frame.origin.x, y:-frame.origin.y)
+        let topRight = CGPoint(x: -frame.origin.x, y:-frame.origin.y)
+        
+        let top = SKNode()
+        top.name = "top"
+        top.physicsBody = SKPhysicsBody(edgeFrom: topLeft, to: topRight)
+        self.addChild(top)
+        
+        
+        topPaddle = self.childNode(withName: "topPaddle") as! SKSpriteNode
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        
+        
+        
+        topPaddle.physicsBody?.categoryBitMask = paddleCategory
+        ball.physicsBody?.categoryBitMask = ballCategory
+        top.physicsBody?.categoryBitMask = topCategory
+        
+        ball.physicsBody?.contactTestBitMask = topCategory|paddleCategory
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        //print(contact.bodyA)
+        //print(contact.bodyB)
+        
+        if contact.bodyA.categoryBitMask == topCategory{
+            changePaddle(node: topPaddle)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+    }
+    func changePaddle(node: SKSpriteNode){
+        if node.color == .yellow{
+            node.removeAllActions()
+            node.removeFromParent()
+        }
+        node.color = .yellow
     }
 }
